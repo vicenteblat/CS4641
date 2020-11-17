accd302c7be3f24286230f6b1306fd05dd6323bf
vicenteblat <blat98v@gmail.com>
2020-11-16 22:47:58 -0500

    Removed lambda expression from NN

--- HW04/template_files/NN.py
+++ HW04/template_files/NN.py
@@ -15,10 +15,9 @@ to train a 2 fully connected layer neural net. We are going to buld the neural n
 
 
 class dlnet:
-
-    def __init__(self, x, y, lr = 0.003):
+    def __init__(self, x, y, lr=0.003):
         '''
-        This method inializes the class, its implmented for you. 
+        This method inializes the class, its implmented for you.
         Args:
             x: data
             y: labels
@@ -30,138 +29,130 @@ class dlnet:
             lr: learning rate
             sam: number of training samples we have
 
-        '''        
-        self.X=x # features
-        self.Y=y # ground truth labels
+        '''
+        self.X = x  # features
+        self.Y = y  # ground truth labels
 
-        self.Yh=np.zeros((1,self.Y.shape[1])) # estimated labels
-        self.dims = [30, 15, 1] # dimensions of different layers
+        self.Yh = np.zeros((1, self.Y.shape[1]))  # estimated labels
+        self.dims = [30, 15, 1]  # dimensions of different layers
 
-        self.param = { } # dictionary for different layer variables
-        self.ch = {} # cache for holding variables during forward propagation to use them in back prop
+        self.param = {}  # dictionary for different layer variables
+        self.ch = {}  # cache for holding variables during forward propagation to use them in back prop
         self.loss = []
 
-        self.lr=lr # learning rate
-        self.sam = self.Y.shape[1] # number of training samples we have
+        self.lr = lr  # learning rate
+        self.sam = self.Y.shape[1]  # number of training samples we have
         self._estimator_type = 'classifier'
-        self.neural_net_type = "Relu -> Sigmoid" #can change it to "Tanh -> Sigmoid" 
+        self.neural_net_type = "Tanh -> Sigmoid"  # can change it to "Tanh -> Sigmoid"
 
-    def nInit(self): 
+    def nInit(self):
         '''
-        This method initializes the neural network variables, its already implemented for you. 
+        This method initializes the neural network variables, its already implemented for you.
         Check it and relate to mathematical the description above.
         You are going to use these variables in forward and backward propagation.
-        '''   
+        '''
         np.random.seed(1)
-        self.param['theta1'] = np.random.randn(self.dims[1], self.dims[0]) / np.sqrt(self.dims[0]) 
-        self.param['b1'] = np.zeros((self.dims[1], 1))        
-        self.param['theta2'] = np.random.randn(self.dims[2], self.dims[1]) / np.sqrt(self.dims[1]) 
-        self.param['b2'] = np.zeros((self.dims[2], 1))                
-        
-    
+        self.param['theta1'] = np.random.randn(self.dims[1], self.dims[0]) / np.sqrt(self.dims[0])
+        self.param['b1'] = np.zeros((self.dims[1], 1))
+        self.param['theta2'] = np.random.randn(self.dims[2], self.dims[1]) / np.sqrt(self.dims[1])
+        self.param['b2'] = np.zeros((self.dims[2], 1))
 
     def Relu(self, u):
         '''
-        In this method you are going to implement element wise Relu. 
-        Make sure that all operations here are element wise and can be applied to an input of any dimension. 
+        In this method you are going to implement element wise Relu.
+        Make sure that all operations here are element wise and can be applied to an input of any dimension.
         Input: u of any dimension
-        return: Relu(u) 
+        return: Relu(u)
         '''
-        relu = np.vectorize(lambda x: max(0, x))
-        o = relu(u)
+        o = np.maximum(u, 0)
         return o
 
-    def Sigmoid(self, u): 
+    def Sigmoid(self, u):
         '''
-        In this method you are going to implement element wise Sigmoid. 
-        Make sure that all operations here are element wise and can be applied to an input of any dimension. 
+        In this method you are going to implement element wise Sigmoid.
+        Make sure that all operations here are element wise and can be applied to an input of any dimension.
         Input: u of any dimension
-        return: Sigmoid(u) 
+        return: Sigmoid(u)
         '''
-        sigmoid = np.vectorize(lambda x: 1 / (1 + np.exp(-x)))
-        o = sigmoid(u)
+        o = 1 / (1 + np.exp(-u))
         return o
-
 
     def Tanh(self, u):
         '''
-        In this method you are going to implement element wise Tanh. 
-        Make sure that all operations here are element wise and can be applied to an input of any dimension. 
+        In this method you are going to implement element wise Tanh.
+        Make sure that all operations here are element wise and can be applied to an input of any dimension.
         Input: u of any dimension
-        return: Tanh(u) 
+        return: Tanh(u)
         '''
-        tanh = np.vectorize(lambda x: (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x)))
-        o = tanh(u)
+        o = (np.exp(u) - np.exp(-u)) / (np.exp(u) + np.exp(-u))
         return o
-    
-    
+
     def dRelu(self, u):
         '''
-        This method implements element wise differentiation of Relu.  
+        This method implements element wise differentiation of Relu.
         Input: u of any dimension
-        return: dRelu(u) 
+        return: dRelu(u)
         '''
-        u[u<=0] = 0
-        u[u>0] = 1
+        u[u <= 0] = 0
+        u[u > 0] = 1
         return u
-
 
     def dSigmoid(self, u):
         '''
-        This method implements element wise differentiation of Sigmoid.  
+        This method implements element wise differentiation of Sigmoid.
         Input: u of any dimension
-        return: dSigmoid(u) 
+        return: dSigmoid(u)
         '''
-        o = 1/(1+np.exp(-u))
-        do = o * (1-o)
+        o = 1 / (1 + np.exp(-u))
+        do = o * (1 - o)
         return do
 
     def dTanh(self, u):
         '''
-        This method implements element wise differentiation of Tanh. 
+        This method implements element wise differentiation of Tanh.
         Input: u of any dimension
-        return: dTanh(u) 
+        return: dTanh(u)
         '''
         o = np.tanh(u)
-        return 1-o**2
-    
-    
-    def nloss(self,y, yh):
+        return 1 - o ** 2
+
+    def nloss(self, y, yh):
         '''
-        In this method you are going to implement Cross Entropy loss. 
+        In this method you are going to implement Cross Entropy loss.
         Refer to the description above and implement the appropriate mathematical equation.
         Input: y 1xN: ground truth labels
                yh 1xN: neural network output after Sigmoid
 
-        return: CE 1x1: loss value 
+        return: CE 1x1: loss value
         '''
         CE = -1 * np.mean((np.multiply(y, np.log(yh)) + np.multiply((1 - y), np.log(1 - yh))))
         return CE
-
 
     def forward(self, x):
         '''
         Fill in the missing code lines, please refer to the description for more details.
         Check nInit method and use variables from there as well as other implemented methods.
         Refer to the description above and implement the appropriate mathematical equations.
-        do not change the lines followed by #keep. 
+        do not change the lines followed by #keep.
         '''
-        self.ch['X'] = x #keep
+        self.ch['X'] = x  # keep
         u1 = np.dot(self.param['theta1'], self.ch['X']) + self.param['b1']
-        o1 = self.Relu(u1)
-        self.ch['u1'], self.ch['o1'] = u1, o1 #keep
+        if self.neural_net_type == "Relu -> Sigmoid":
+            o1 = self.Relu(u1)
+        else:
+            o1 = self.Tanh(u1)
+        self.ch['u1'], self.ch['o1'] = u1, o1  # keep
         u2 = np.dot(self.param['theta2'], self.ch['o1']) + self.param['b2']
         o2 = self.Sigmoid(u2)
-        self.ch['u2'], self.ch['o2'] = u2, o2 #keep
-        return o2 #keep
-
+        self.ch['u2'], self.ch['o2'] = u2, o2  # keep
+        return o2  # keep
 
     def backward(self, y, yh):
         '''
         Fill in the missing code lines, please refer to the description for more details
         You will need to use cache variables, some of the implemented methods, and other variables as well
         Refer to the description above and implement the appropriate mathematical equations.
-        do not change the lines followed by #keep.  
+        do not change the lines followed by #keep.
         '''
         dLoss_o2 = - (np.divide(y, yh) - np.divide(1 - y, 1 - yh)) / y.shape[1]  # partial l by partial o2
 
@@ -178,7 +169,10 @@ class dlnet:
         dLoss_o1 = np.dot(self.param["theta2"].T, dLoss_u2)  # partial l by partial o1
 
         # Implement equations for getting derivative of loss w.r.t u1, theta1 and b1
-        do1_u1 = self.dRelu(self.ch['u1'])
+        if self.neural_net_type == "Relu -> Sigmoid":
+            do1_u1 = self.dRelu(self.ch['u1'])
+        else:
+            do1_u1 = self.dTanh(self.ch['u1'])
         dLoss_u1 = np.multiply(dLoss_o1, do1_u1)  # partial l by partial u1
         dLoss_theta1 = np.matmul(dLoss_u1, self.ch['X'].T)  # partial l by partial theta1
         dLoss_b1 = np.matmul(dLoss_u1, np.ones((dLoss_u1.shape[1], 1)))  # partial l by partial b1
@@ -195,52 +189,50 @@ class dlnet:
         self.param["b1"] = self.param["b1"] - self.lr * dLoss_b1  # keep
         return dLoss_theta2, dLoss_b2, dLoss_theta1, dLoss_b1
 
-
-    def gradient_descent(self, x, y, iter = 60000):
+    def gradient_descent(self, x, y, iter=60000):
         '''
-        This function is an implementation of the gradient decent algorithm 
+        This function is an implementation of the gradient decent algorithm
         '''
         self.nInit()
         for i in range(iter):
             yh = self.forward(x)
+            loss = self.nloss(y, yh)
+            self.loss.append(loss)
             if i % 2000 == 0:
-                loss = self.nloss(y, yh)
-                self.loss.append(loss)
                 print('Loss after iteration %d: %6f' % (i, loss))
             _, _, _, _ = self.backward(y, yh)
 
-
-    #bonus for undergraduate students 
-    def stochastic_gradient_descent(self, x, y, iter = 60000):
+    # bonus for undergraduate students
+    def stochastic_gradient_descent(self, x, y, iter=60000):
         '''
         This function is an implementation of the stochastic gradient decent algorithm
 
-        Note: 
-        1. SGD loops over all examples in the dataset one by one and learns a gradient from each example 
-        2. One iteration here is one round of forward and backward propagation on one example of the dataset. 
-           So if the dataset has 1000 examples, 1000 iterations will constitute an epoch 
+        Note:
+        1. SGD loops over all examples in the dataset one by one and learns a gradient from each example
+        2. One iteration here is one round of forward and backward propagation on one example of the dataset.
+           So if the dataset has 1000 examples, 1000 iterations will constitute an epoch
         3. Append loss after every 2000 iterations for plotting loss plots
-        4. It is fine if you get a noisy plot since learning on one example at a time adds variance to the 
+        4. It is fine if you get a noisy plot since learning on one example at a time adds variance to the
            gradients learnt
-        5. You can use SGD with any neural net type 
+        5. You can use SGD with any neural net type
         '''
         self.nInit()
-        for i in range(iter):
+        for i in range(iter + 1):
             idx = i % x.shape[1]
             xs = np.expand_dims(x[:, idx], axis=1)
             ys = np.expand_dims(y[:, idx], axis=1)
             yh = self.forward(xs)
-            if i % 2000 == 0:
+            if (i + 1) % 2000 == 0:
                 loss = self.nloss(ys, yh)
                 self.loss.append(loss)
-                print('Loss after iteration %d: %6f' % (i, loss))
+                print('Loss after iteration %d: %6f' % (i + 1, loss))
             _, _, _, _ = self.backward(ys, yh)
 
-
-    def predict(self, x): 
+    def predict(self, x):
         '''
         This function predicts new data points
         Its implemented for you
         '''
         Yh = self.forward(x)
         return np.round(Yh).squeeze()
+
